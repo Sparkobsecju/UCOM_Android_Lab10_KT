2ff2517f7c47244ba29a30ea5290990027dbaf72
Sivan Chen <ryan1825114@gmail.com>
2024-06-13 17:04:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab10_kt/DemoPreferenceActivity.kt
@@ -0,0 +1,10 @@
+package com.example.lab10_kt
+
+import android.os.Bundle
+import android.preference.PreferenceActivity
+
+class DemoPreferenceActivity:PreferenceActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/lab10_kt/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lab10_kt
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu!!.add("使用者偏好設定")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val intent = Intent(this, DemoPreferenceActivity::class.java)
+        startActivity(intent)
+        return super.onOptionsItemSelected(item)
+    }
+}
