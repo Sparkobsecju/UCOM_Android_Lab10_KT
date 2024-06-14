2174cb24d2efe095259d9388d2b84304642e99a0
Sivan Chen <ryan1825114@gmail.com>
2024-06-14 09:14:42 +0800

    add Preferences for PreferenceActivity (addPreferencesFromResource())

--- app/src/main/java/com/example/lab10_kt/DemoPreferenceActivity.kt
+++ app/src/main/java/com/example/lab10_kt/DemoPreferenceActivity.kt
@@ -3,8 +3,9 @@ package com.example.lab10_kt
 import android.os.Bundle
 import android.preference.PreferenceActivity
 
-class DemoPreferenceActivity:PreferenceActivity() {
+class DemoPreferenceActivity : PreferenceActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.system_default)
     }
 }
